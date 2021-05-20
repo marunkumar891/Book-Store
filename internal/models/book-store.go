d04eca6f35227d1840545c20960686b9e3acb516
ArunKumar <arun.i@xpresslane.in>
2021-05-20 12:04:06 +0530

    updated gorm mappings

--- internal/models/book-store.go
+++ internal/models/book-store.go
@@ -1,10 +1,15 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 const Table_Book = "books"
 
 type Book struct {
+	gorm.Model
 	Id      uint64    `json:"id" gorm:"primary_key"`
 	Author  string    `json:"author"`
 	AddedAt time.Time `json:"added_at"`
