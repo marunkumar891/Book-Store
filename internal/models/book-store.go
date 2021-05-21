ce56ed93af8b167ebda62a3d41db4c1c672896ef
ArunKumar <arun.i@xpresslane.in>
2021-05-21 22:45:00 +0530

    refactored db primary_key

--- internal/models/book-store.go
+++ internal/models/book-store.go
@@ -2,15 +2,13 @@ package models
 
 import (
 	"time"
-
-	"github.com/jinzhu/gorm"
 )
 
 const Table_Book = "books"
 
 type Book struct {
-	gorm.Model
-	Id          string    `json:"id" gorm:"column:id;primary_key;unique"`
+	//gorm.Model
+	Id          string    `json:"id" gorm:"column:id;primaryKey"`
 	BookName    string    `json:"book_name" gorm:"column:book_name"`
 	Author      string    `json:"author" gorm:"column:author"`
 	IsAvailable bool      `json:"is_available" gorm:"column:is_available"`
